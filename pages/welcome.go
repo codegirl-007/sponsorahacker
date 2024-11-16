60188b064cb47e9d1324aece91444d2dd13f9adf
Stephanie Gredell <s.raide@gmail.com>
2024-11-16 04:31:39 -0500

    fix passing isLoggedIn to header.html

--- pages/welcome.go
+++ pages/welcome.go
@@ -1,12 +1,17 @@
 package pages
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"sponsorahacker/utils"
 )
 
 func Welcome(c *gin.Context) {
+	isLoggedIn := utils.CheckIfLoggedIn(c)
+	fmt.Println("isLoggedIn:", isLoggedIn)
 	c.HTML(http.StatusOK, "welcome.html", gin.H{
-		"title": "Sponsor A Hacker",
+		"title":      "Sponsor A Hacker",
+		"isLoggedIn": isLoggedIn,
 	})
 }
